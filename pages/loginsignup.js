44f24bce977df6f55d253e1b1060b7016f9b5464
Jaycode01 <josephlamidi01@gmail.com>
2024-11-17 16:45:55 +0100

    Making Arrangement of files and setting the navbar well

---
+++ pages/loginsignup.js
@@ -0,0 +1,8 @@
+// Navbar toggle
+
+const toggleButton = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+toggleButton.addEventListener('click', () => {
+  navLinks.classList.toggle('active'); // Toggle the sliding effect
+});
