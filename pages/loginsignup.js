1c4af4a61fa74ad9ccb2e46baf8427ba65ed8d50
Ekundayo Mayowa T. <ekundayomayor@gmail.com>
2024-11-23 20:06:27 +0100

    login-page and re-arrangment

--- pages/loginsignup.js
+++
@@ -1,8 +0,0 @@
-// Navbar toggle
-
-const toggleButton = document.querySelector('.menu-toggle');
-const navLinks = document.querySelector('.nav-links');
-
-toggleButton.addEventListener('click', () => {
-  navLinks.classList.toggle('active'); // Toggle the sliding effect
-});
