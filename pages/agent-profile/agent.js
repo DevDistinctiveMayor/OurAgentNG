8baef755b06e7a90354b2116229cd04ea9ba199e
chineducode <chinedunnaemeka2@gmail.com>
2025-01-03 22:32:05 +0100

    agent page modified

--- pages/agent-profile/agent.js
+++ pages/agent-profile/agent.js
@@ -36,3 +36,15 @@ nums.forEach((num, index) => {
         activeNum = index
     })
 })
+
+//For Nav
+const menuToggle = document.querySelector(".menu-toggle");
+const menu = document.querySelector(".nav-links");
+const content = document.querySelector(".content"); // Target content-wrapper
+
+menuToggle.addEventListener("click", () => {
+  menuToggle.classList.toggle("open");
+  menu.classList.toggle("active");
+  content.classList.toggle("content-blur"); // Add blur effect to the content
+  document.body.classList.toggle("disable-scroll"); // Prevent scrolling on the whole page
+});
